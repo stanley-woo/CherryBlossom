5ee061f52f5cd98709356f56f9f2bcc24ecae2d3
Stanley Woo <46428542+stanley-woo@users.noreply.github.com>
2026-05-07 11:34:30 +0800

    Need to work on Description part later.

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_button.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -8,6 +12,9 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 4;
+  int selectedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +28,7 @@ class _DetailPageState extends State<DetailPage> {
               right: 0,
               child: Container(
                 width: double.maxFinite,
-                height: 300,
+                height: 350,
                 decoration: BoxDecoration(
                   image: DecorationImage(image: AssetImage("img/mountain.jpeg"), fit: BoxFit.cover),
                 ),
@@ -40,8 +47,78 @@ class _DetailPageState extends State<DetailPage> {
                   ],
                 )
               ),
+              Positioned(
+                top: 320,
+                child: Container(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    )
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AppLargeText(text: "Yosemite", color: Colors.black54.withAlpha(80)),
+                          AppLargeText(text: "\$ 250", color: AppColors.mainColor,),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Icon(Icons.location_on, color: AppColors.mainColor,),
+                          SizedBox(width: 5,),
+                          AppText(text: "USA, California", color: AppColors.textColor1,),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: List.generate(5, (index) {
+                              return Icon(Icons.star, color: gottenStars > index? AppColors.starColor : AppColors.textColor2);
+                            }),
+                          ),
+                          SizedBox(width: 5,),
+                          AppText(text: "(4.0)", color: AppColors.textColor2,)
+                        ],
+                      ),
+                      SizedBox(height: 25,),
+                      AppLargeText(text: "People", color: Colors.black.withAlpha(90), size: 20,),
+                      SizedBox(height: 5,),
+                      AppText(text: "Number of People in Your Group", color: AppColors.mainTextColor,),
+                      SizedBox(height: 10,),
+                      Wrap(
+                        children: List.generate(5, (index) {
+                          return InkWell(
+                            onTap: () {
+                              setState(() {
+                                selectedIndex = index;
+                              });
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(right: 10), 
+                              child: AppButton(
+                                size: 50, 
+                                color: selectedIndex == index? Colors.white : Colors.black, 
+                                backgroundColor: selectedIndex == index? Colors.black : AppColors.buttonBackground, 
+                                borderColor: selectedIndex == index? Colors.black : AppColors.buttonBackground, 
+                                text: (index+1).toString())));
+                        }),
+                      )
+                    ],
+                  ),
+                )
+              )
           ],
-        ),
+        ), 
       )
     );
   }
---
+++ lib/widgets/app_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
+
+class AppButton extends StatelessWidget {
+  final double size;
+  final Color color;
+  final Color backgroundColor;
+  final Color borderColor;
+  final String? text;
+  final IconData? icon;
+  bool? isIcon;
+
+  AppButton({
+    super.key,
+    required this.size,
+    required this.color,
+    required this.backgroundColor,
+    required this.borderColor,
+    this.text = "Julie",
+    this.icon,
+    this.isIcon = false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: borderColor,
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(15),
+        color: backgroundColor,
+      ),
+      child: isIcon == false ? Center(child: AppText(text: text!, color: color)) : Center(child: Icon(icon, color: color)),
+    );
+  }
+}
